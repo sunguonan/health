c7eeb606a836a34fedda4ae9ea085106ca024a7b
孙国楠 <1559039169@qq.com>
2023-01-09 20:59:00 +0800

    zz

---
+++ src/main/java/com/ydlclass/health/dao/SetMealDao.java
@@ -0,0 +1,23 @@
+package com.ydlclass.health.dao;
+
+import com.github.pagehelper.Page;
+import com.ydlclass.health.common.pojo.Setmeal;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Map;
+
+/**
+ * @author sunGuoNan
+ * @version 1.0
+ */
+@Mapper
+public interface SetMealDao {
+
+    void add(@Param("setmeal") Setmeal setmeal);
+
+
+    void buildSetMealAndCheckGroupId(@Param("setmealAndCheckGroupData") Map<String, Integer> setmealAndCheckGroupData);
+
+    Page<Setmeal> findPage(@Param("queryString") String queryString);
+}
---
+++ src/main/java/com/ydlclass/health/service/SetMealService.java
@@ -0,0 +1,16 @@
+package com.ydlclass.health.service;
+
+import com.ydlclass.health.common.entity.PageResult;
+import com.ydlclass.health.common.entity.QueryPageBean;
+import com.ydlclass.health.common.pojo.Setmeal;
+
+/**
+ * @author sunGuoNan
+ * @version 1.0
+ */
+public interface SetMealService {
+
+    void add(Integer[] checkgroupIds, Setmeal setmeal);
+
+    PageResult findPage(QueryPageBean queryPageBean);
+}
